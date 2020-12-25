b01cced12502dab6e1894e492e272729f75466f5
harny-wu <51947541+harny-wu@users.noreply.github.com>
2020-12-25 20:53:21 +0800

    1.user新增图片字段

--- src/main/java/com/gudt/imis/clubmanageis/service/UserService.java
+++ src/main/java/com/gudt/imis/clubmanageis/service/UserService.java
@@ -6,6 +6,7 @@ import com.gudt.imis.clubmanageis.model.result.Result;
 import com.gudt.imis.clubmanageis.model.vo.UserClubRoleVo;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
@@ -13,4 +14,5 @@ public interface UserService {
     Result<User> updateUserInfo(User user);
     Result<String> updateUserAvatar(int userId, MultipartFile uploadImg);
     Result<UserClubRoleVo> getUserClubAndRole(int userId,int clubId);
+    Result<String>updateUserClubRole(int userId1,int userId2,int clubId,int role);
 }
